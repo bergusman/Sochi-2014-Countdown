8d4d9b340cc7ebf5773d686b8dc181fe3d1efb63
Vitaly Berg <bergusman@gmail.com>
2014-01-24 03:05:11 +0400

    Initial Commit

---
+++ Sochi 2014 Countdown/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  Sochi 2014 Countdown
+//
+//  Created by Vitaly Berg on 24/01/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sochi 2014 Countdown/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  Sochi 2014 Countdown
+//
+//  Created by Vitaly Berg on 24/01/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
